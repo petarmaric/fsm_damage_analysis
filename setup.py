d12cea97ed6bfcffb4e2bdd149e07a387376fe84
Petar Marić <petar.maric@gmail.com>
2019-09-02 01:59:35 +0200

    Update packaging info for 1.0.2 release

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='fsm_damage_analysis',
-    version='1.0.1',
+    version='1.0.2',
     url='https://github.com/petarmaric/fsm_damage_analysis',
     license='BSD',
     author='Petar Maric',
